d4690d6d14d447485594cfd392a27243250565a3
gustavo.dssousa@aluno.uepa.br <gustavo.dssousa@aluno.uepa.br>
2024-02-13 16:33:14 -0300

    Creating DepartmentDao and SellerDao Interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+	public void insert(Department obj);
+
+	public void update(Seller obj);
+
+	public void deleteById(Seller id);
+
+	Seller findById(Integer id);
+
+	List<Seller> findAll();
+}
